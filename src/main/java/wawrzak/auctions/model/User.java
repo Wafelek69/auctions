cb4285193d05236fafb8ebf8ecbf5b43c7c9cbc4
Wafelek <korni888@yahoo.pl>
2019-11-30 22:17:39 +0100

    2

---
+++ src/main/java/wawrzak/auctions/model/User.java
@@ -0,0 +1,102 @@
+package wawrzak.auctions.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Email
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @Size(min = 6)
+    @NotEmpty
+    @NotNull
+    private String password;
+
+    @Size(min = 3)
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @Size(min = 3)
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
+    private String wojewodztwo;
+
+    @Size(min = 3)
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
+    private String city;
+
+    private boolean active;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWojewodztwo() {
+        return wojewodztwo;
+    }
+
+    public void setWojewodztwo(String wojewodztwo) {
+        this.wojewodztwo = wojewodztwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
